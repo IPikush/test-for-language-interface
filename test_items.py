d905dde9ffb2b360d1b7d42b1c3c728b5d1361e7
IPikush <59697676+IPikush@users.noreply.github.com>
2020-01-15 09:49:11 +0200

    update test for language interface

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@
 import pytest
 from selenium import webdriver
 import time
-import math
+from selenium.common.exceptions import NoSuchElementException
 
 
 
@@ -10,8 +10,15 @@ def test_button_availability(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(5)
+    try:
+        browser.find_element_by_css_selector("button.btn-add-to-basket")
+        result = True
+    except NoSuchElementException:
+        result = False
+
+    assert result == True, "button is not available"
     
-    assert (browser.find_element_by_css_selector("button.btn-add-to-basket"))   , "button is not available"
+    
     
     
     
